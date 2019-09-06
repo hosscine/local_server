3c99094bf790e633dcb72d00eba5f93dcdd99252
hosscine <futagamirentaro@gmail.com>
2019-09-06 19:22:51 +0900

    fix app.js to read vue template

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 const fs = require('fs')
 
-const vue_template = fs.readFileSync("assets/vue_template.txt", {encoding: "utf-8"})
+const vue_template = fs.readFileSync("assets/vue_template.vue", {encoding: "utf-8"})
 
 const server = app.listen(80, function(){
   console.log("Node.js is listening to PORT:" + server.address().port)
